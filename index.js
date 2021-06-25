cce539cca7c6499c75ded7aaa3ad3333158d4794
Mizanur Rahman <mizzanur007@gmail.com>
2021-06-25 20:43:48 +0600

    changed .env

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const port = process.env.PORT || 7979;
+const port = 5000;
 
 app.get('/', (req, res) => {
   res.send('Welcome to app');
@@ -21,29 +21,18 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
 });
 client.connect((err) => {
-  const bookingCollection = client
-    .db(`${process.env.DBNAME}`)
-    .collection('destinations');
-  const ordersCollection = client
-    .db(`${process.env.DBNAME}`)
-    .collection('orders');
-
-  app.post('/addBooking', (req, res) => {
-    bookingCollection
-      .insertOne(req.body)
-      .then((result) => res.send(!!result.insertedCount));
-  });
-
-  app.post('/addOrder', (req, res) => {
-    orderCollection
-      .insertOne(req.body)
-      .then((result) => res.send(!!result.insertedCount));
-  });
+  const bookingCollection = client.db('airFlight').collection('destinations');
+  const ordersCollection = client.db('airFlight').collection('orders');
 
   app.get('/bookings', (req, res) => {
     bookingCollection.find({}).toArray((err, docs) => res.send(docs));
   });
 
+  // app.post('/addBooking', (req, res) => {
+  //   bookingCollection
+  //     .insertOne(req.body)
+  //     .then((result) => res.send(!!result.insertedCount));
+  // });
   app.get('/orders', (req, res) => {
     adminsCollection.find({ email: req.query.email }).toArray((err, docs) => {
       if (docs.length) {
@@ -54,6 +43,12 @@ client.connect((err) => {
           .toArray((err, docs) => res.send(docs));
       }
     });
+  });
+
+  app.post('/addOrder', (req, res) => {
+    orderCollection
+      .insertOne(req.body)
+      .then((result) => res.send(!!result.insertedCount));
   });
 });
 
